2e43bde85e83e31deb2bbe9e0984dab3545e365c
NS Lamaaa <maxberg.pp@gmail.com>
2026-06-20 18:42:10 +0200

    Updated config.lua

--- ROS FiveM/config.lua
+++ ROS FiveM/config.lua
@@ -4,6 +4,6 @@ Config.title = "911 System" --title
 Config.subject = "~b~Information:" --subject
 Config.msg = "Du hast dich überschlagen. Rufe einen Rettungswagen, und warte im Fahrzeug auf Hilfe." --message
 Config.icon = "CHAR_CALL911" --icon can be seen here: https://wiki.rage.mp/wiki/Notification_Pictures
-Config.maxDisableTime = 300 -- Dont Touch!!
+Config.maxDisableTime = 300 -- Dont Touch!!!
 
 --Standard Language is DE, You can Change Everything.
